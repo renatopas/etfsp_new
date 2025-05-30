1e3bd4ff3c0e0c4d2a440e802b93a3c8723bb380
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2025-05-30 04:06:20 -0300

    feat: initial detalhe_exaluno impl

---
+++ src/routes/detalhe_exaluno/+page.server.ts
@@ -0,0 +1,62 @@
+import { db } from '$lib/db.js'
+import { error } from '@sveltejs/kit'
+
+interface DadosAluno {
+  Nome: string;
+  ID: number;
+  Apelidos: string;
+  TipoCurso: string;
+  Curso: string;
+  AnoInicio: number;
+  AnoTermino: number;
+  Email: string;
+  HomePage: string;
+  ICQ: string;
+  DtCadastro: string;
+  DtAtualizacao: string;
+  Comentarios: string;
+  ComoEncontrou: string;
+  ComoEncontrouExtra: string;
+  DadoPubl: string;
+  NomeMiniaturaPes: string;
+  QtdFotos: number;
+}
+
+async function getDadosAluno(id: number): Promise<Partial<DadosAluno> | undefined> {
+  return new Promise((res, rej) => {
+    db.get(
+      "SELECT \
+        ID, Nome, \
+        Apelidos, Curso, \
+        AnoInicio, AnoTermino, \
+        Email, HomePage, ICQ, \
+        DtCadastro, DtAtualizacao, \
+        Comentarios, \
+        ComoEncontrou, \
+        ComoEncontrouExtra, \
+        DadoPubl \
+      FROM ExAlunos \
+      WHERE ID = ?",
+      id,
+      (err, row?: Partial<DadosAluno>) => {
+        if (err) {
+          rej(err)
+          return;
+        }
+        res(row)
+      }
+    )
+  })
+}
+
+export const load = async ({ url }) => {
+  const idParam = url.searchParams.get("id")
+  if (idParam === null || Number.isNaN(parseInt(idParam))) {
+    error(401)
+  }
+  const dados = await getDadosAluno(parseInt(idParam))
+  if (!dados) {
+    error(404)
+  }
+  return dados
+}
---
+++ src/routes/detalhe_exaluno/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  let { data } = $props()
+</script>
+
+<h1>Dados detalhados de ex-aluno</h1>
+<p>
+
+  <!-- TODO: foto
+  <a href=lista_foto.asp?idExAluno=<%=Id%>>
+    <img border="0" src="Fotos/<%=RS("NomeMiniaturaPes")%>" alt="Veja todas as <%=RS("QtdFotos")%> fotos do ex-aluno">
+  </a>
+  -->
+  <br>
+
+  <b>Nome</b>: {data.Nome}{data.Apelidos ? ` - ${data.Apelidos}`: ""}<br>
+  <b>Curso</b>: {data.TipoCurso} <font color="#FF0000"> {data.Curso}
+  </font> {data.AnoInicio} <b> a </b> {data.AnoTermino}
+  <br><b>Email</b>: <a href="mailto:{data.Email}">{data.Email}</a>
+  <br><b>HomePage</b>: <a href="{data.HomePage}" target="_blank">{data.HomePage}</a>
+  {#if data.QtdFotos ?? 0 > 0}
+    <br><a href="lista_foto.asp?idExAluno={data.ID}">Clique aqui para ver as {data.QtdFotos} fotos enviadas</a>
+  {/if}
+
+  <br><b>ICQ #</b>: {data.ICQ}
+  <br><b>Outros dados: </b>{data.DadoPubl}
+  <br><b>Cadastro</b>:&nbsp;&nbsp;&nbsp; {data.DtCadastro}
+  <br><b>Atualização</b>: {data.DtAtualizacao}
+  <br><b>Como nos encontrou</b>: {data.ComoEncontrou}{data.ComoEncontrouExtra ? ` - ${data.ComoEncontrouExtra}`: ""}
+  <br><b>Histórico</b>:
+  <br>{data.Comentarios}
+</p>
