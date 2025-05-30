a815b4da1e4113c2e44e4e69283bffd193695231
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2025-05-30 03:07:28 -0300

    refactor: overhaul lista_foto

--- src/routes/lista_foto/+page.svelte
+++ src/routes/lista_foto/+page.svelte
@@ -7,16 +7,51 @@
   }
 
   let { form } = $props();
-  let rows: Foto[][] = [];
-  for (let i = 0; i < Math.ceil((form?.fotos.length ?? 0) / 5); i++) {
-    rows.push(form?.fotos.slice(i * 5, i * 5 + 5) ?? []);
+
+  let rows = $derived.by(() => {
+    let tmp: Foto[][] = [];
+    let formData = form;
+    for (let i = 0; i < Math.ceil((formData?.fotos.length ?? 0) / 5); i++) {
+      tmp.push(formData?.fotos.slice(i * 5, i * 5 + 5) ?? []);
+    }
+    return tmp;
+  });
+
+  function validarForm(this: HTMLFormElement, e: SubmitEvent) {
+    let alerted = false;
+    const validarAno = (elem: HTMLInputElement) => {
+      console.assert(elem instanceof HTMLInputElement);
+      if (!elem.value) {
+        return;
+      }
+      const parsed = parseInt(elem.value);
+      if (parsed < 1960 || parsed > 2040) {
+        if (!alerted) alert("Por favor digite um ano entre 1960 e 2040");
+      }
+      if (!Number.isSafeInteger(parsed)) {
+        if (!alerted) alert("Por favor digite um número nos campos de ano");
+      }
+      e.preventDefault();
+    };
+    validarAno(this.range_ano_foto_start);
+    validarAno(this.range_ano_foto_end);
+    validarAno(this.range_ano_formatura_start);
+    validarAno(this.range_ano_formatura_end);
   }
 </script>
 
 <div id="mainbody2">
   <h1>Lista fotos</h1>
   <p><a href="cadfoto.asp"><big>Envie mais uma foto</big></a></p>
-  <form method="POST" name="form01" id="form01" use:enhance>
+  <form
+    method="POST"
+    name="form01"
+    id="form01"
+    use:enhance={() => (o) => {
+      o.update({ reset: false });
+    }}
+    onsubmit={validarForm}
+  >
     <table
       width="100%"
       border="0"
@@ -38,39 +73,36 @@
               maxlength="20"
               size="20"
               tabindex="1"
-              value={form?.params.titulo}
             />
           </td>
           <td
             >Curso<font size="2" face="Verdana"
               ><strong>
-                <!-- TODO: bindar esses valores -->
                 <select name="curso" tabindex="2">
-                  <option selected={form?.params.curso == ""} value=""
-                    >Todos</option
-                  >
-                  <option selected={form?.params.curso == "PRD"}>PRD</option>
-                  <option selected={form?.params.curso == "TEL"}>TEL</option>
-                  <option selected={form?.params.curso == "ELO"}>ELO</option>
-                  <option selected={form?.params.curso == "ELE"}>ELE</option>
-                  <option selected={form?.params.curso == "EDI"}>EDI</option>
-                  <option selected={form?.params.curso == "MEC"}>MEC</option>
-                  <option selected={form?.params.curso == "INF"}>INF</option>
+                  <option selected value="">Todos</option>
+                  <option>PRD</option>
+                  <option>TEL</option>
+                  <option>ELO</option>
+                  <option>ELE</option>
+                  <option>EDI</option>
+                  <option>MEC</option>
+                  <option>INF</option>
                 </select></strong
               ></font
             >Apenas carômetro?
             <input
               type="radio"
               value="true"
-              checked={form?.params.isCarometro}
               name="carometro"
-            />Sim
+              id="carometro_sim"
+            /><label for="carometro_sim">Sim</label>
             <input
               type="radio"
               value="false"
-              checked={!form?.params.isCarometro}
+              checked
               name="carometro"
-            />Não
+              id="carometro_nao"
+            /><label for="carometro_nao">Não</label>
           </td>
         </tr>
         <tr>
@@ -82,14 +114,12 @@
               maxlength="4"
               name="range_ano_foto_start"
               size="6"
-              value={form?.params.range_ano_foto_start}
             />e
             <input
               type="text"
               name="range_ano_foto_end"
               maxlength="4"
               size="6"
-              value={form?.params.range_ano_foto_end}
             />
           </td>
           <td
@@ -99,14 +129,12 @@
               name="range_ano_formatura_start"
               maxlength="4"
               size="6"
-              value={form?.params.range_ano_formatura_start}
             />e
             <input
               type="text"
               name="range_ano_formatura_end"
               maxlength="4"
               size="6"
-              value={form?.params.range_ano_formatura_end}
             />
           </td>
         </tr>
@@ -122,18 +150,20 @@
     cellspacing="1"
     id="tablefotos"
   >
-    <caption
-      >Encontradas {form?.fotos.length ?? 0} fotos.
-
-      <!-- <a onclick={() => exibirTodas()}>Clique aqui para exibir todas.</a> -->
-    </caption>
+    <caption>Encontradas {form?.fotos.length ?? 0} fotos. </caption>
     <tbody>
       {#each rows as row}
         <tr>
           {#each row as img}
             <td style="text-align: center;">
-              <a href="/Fotos/{img.NomeArqStored}">
-                <img src="/Fotos/{img.NomeMiniaturaStored}" />
+              <a
+                href="/Fotos/{img.NomeArqStored}"
+                aria-label="Foto enviada por usuário"
+              >
+                <img
+                  src="/Fotos/{img.NomeMiniaturaStored}"
+                  alt="Foto enviada por usuário"
+                />
               </a>
               <br />
               {img.TituloFoto}
